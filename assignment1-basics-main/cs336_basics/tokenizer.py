8367d9fdee322233e5abbec863adb52ca1157885
AmandaSakura <1298018274@qq.com>
2026-03-08 23:09:19 +0800

    tokenizer_update

---
+++ assignment1-basics-main/cs336_basics/tokenizer.py
@@ -0,0 +1,7 @@
+from typing import Any
+def get_tokenizer(
+    vocab: dict[int, bytes],
+    merges: list[tuple[bytes, bytes]],
+    special_tokens: list[str] | None = None,
+) -> Any:
+    raise NotImplementedError
