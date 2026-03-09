4aae7877267216bc057e41274f3219d96e056ae1
AmandaSakura <1298018274@qq.com>
2026-03-09 19:19:17 +0800

    update

--- assignment1-basics-main/cs336_basics/tokenizer.py
+++ assignment1-basics-main/cs336_basics/tokenizer.py
@@ -1,3 +1,6 @@
+from typing import Iterable, Iterator
+
+
 class Tokenizer:
     def __init__(self, vocab, merges, special_tokens=None):
         self.vocab = vocab
@@ -5,4 +8,13 @@ class Tokenizer:
         self.special_tokens = special_tokens
     
     def from_files(cls, vocab_filepath, merges_filepath, special_tokens=None):
+        pass
+
+    def encode(self, text: str) -> list[int]:
+        pass
+
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        pass
+
+    def decode(self, ids: list[int]) -> str:
         pass
