ec4e4696b555cafb6a415acd75877d95d69b1832
万振洋 <amandasakura@mozhenyangdeMacBook-Air-2.local>
2026-03-10 11:43:14 +0800

    pass to ... update

--- assignment1-basics-main/cs336_basics/tokenizer.py
+++ assignment1-basics-main/cs336_basics/tokenizer.py
@@ -7,14 +7,15 @@ class Tokenizer:
         self.merges = merges
         self.special_tokens = special_tokens
     
+    @classmethod
     def from_files(cls, vocab_filepath, merges_filepath, special_tokens=None):
-        pass
+        ...
 
     def encode(self, text: str) -> list[int]:
-        pass
+        ...
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
-        pass
+        ...
 
     def decode(self, ids: list[int]) -> str:
-        pass
+        ...
