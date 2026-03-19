1487875846afb3f20d536f5faa2b3c226ccbd1c3
万振洋 <amandasakura@mozhenyangdeMacBook-Air-2.local>
2026-03-19 18:50:15 +0800

    update

--- assignment1-basics-main/cs336_basics/data.py
+++ assignment1-basics-main/cs336_basics/data.py
@@ -1,11 +1,20 @@
 import numpy.typing as npt
 import torch
+
+# 这个代码的作用是随机采样，因为不可能吃整本书训练
+# 而是反复的从长序列中抽取长度为 context_lenth的小窗口，形成输入x和目标y。
+# 这是假设原始文本已经被 tokenizer 成了一串 token id。
+
 def get_batch(dataset: npt.NDArray, batch_size: int, context_length: int, device: str) -> tuple[torch.Tensor, torch.Tensor]:
+    # 首先是判断起始位置能够随机到的最大值，不然构不成一个 contex_lenght 了
     max_start = len(dataset) - context_length
+    # 然后是在 0 到max 的范围内随机 batch_size 个起始点
     starts = torch.randint(0, max_start, (batch_size,))
+    # 从每个起点的切出 contex 长度范围，转为torch 张量，然后stack 拼接到一起。
     x = torch.stack(
         [torch.tensor(dataset[s : s + context_length], dtype=torch.long) for s in starts]
     )
+    # 同理
     y = torch.stack(
         [torch.tensor(dataset[s + 1 : s + context_length + 1], dtype=torch.long) for s in starts]
     )
